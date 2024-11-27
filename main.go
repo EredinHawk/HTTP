d2115d6ac396da10fe37ac83089d1e209d49242a
EredinHawk <Eredinhawk@gmail.com>
2024-11-27 20:23:07 +0200

    Changed the address

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/EredinHawk/http/get"
 	"github.com/EredinHawk/http/secret"
@@ -19,32 +20,46 @@ func init() {
 // main запускает http сервер с обработчиком, который выполняет HTTP запрос на сервер NASA
 // и возвращает результат в виде URL на изображение.
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		request, err := get.ConstructRequest()
-		if err != nil {
-			log.Fatal(err)
-		}
-		client := http.Client{}
+	server := constructServer()
 
-		response, err := client.Do(request)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer response.Body.Close()
-		
-		result, err := get.GetURL(response)
-		if err != nil {
-			fmt.Fprint(w, err.Error())
-		}
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		w.Header().Add("Content-Type", "application/json")
-		fmt.Fprintf(w, "%s", result)
-	})
-	
-	fmt.Println("Сервер прослушивает входящие запросы.")
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Println("Сервер localhost:8080 запущен и прослушивает входящие запросы...")
+	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("ошибка при запуске сервера (%v)", err)
 	}
+}
+
+// constructServer инициализирует http сервер и эндпоинты
+func constructServer() *http.Server {
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", handler_API)
+
+	srv := &http.Server{
+		Addr:         "localhost:8080",
+		Handler:      router, // HTTP мультиплексор, или по другому роутер
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	return srv
+}
+
+func handler_API(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	request, err := get.ConstructRequest()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+	client := http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+	defer response.Body.Close()
+
+	result, err := get.GetURL(response)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+	fmt.Fprintf(w, "%s", result)
 }
